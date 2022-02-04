af94c16a780d51e1348aaed61212a2e6111f6991
xinzhou0714 <zhouxin0714@gmail.com>
2022-02-04 17:46:42 +0100

    chore(log): log server port when backend started

--- src/main/java/com/xinz/retool/ReToolApplication.java
+++ src/main/java/com/xinz/retool/ReToolApplication.java
@@ -1,13 +1,20 @@
 package com.xinz.retool;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ReToolApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(ReToolApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(ReToolApplication.class, args);
+        SpringApplication app = new SpringApplication(ReToolApplication.class);
+        Environment env =app.run(args).getEnvironment();
+        LOG.info("Backend started successfully！！");
+        LOG.info("Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
     }
 
 }
