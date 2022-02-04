38744ec857804043862e8282c6879e9d41fe8825
xinzhou0714 <zhouxin0714@gmail.com>
2022-02-04 17:30:57 +0100

    init with Spring Boot version 2.4.0

---
+++ src/main/java/com/xinz/retool/ReToolApplication.java
@@ -0,0 +1,13 @@
+package com.xinz.retool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xinz/retool/ReToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinz.retool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
