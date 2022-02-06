4baaeb9df7a6b0f89a644970d1e24c2a1094fdbc
xinzhou0714 <zhouxin0714@gmail.com>
2022-02-06 17:37:36 +0100

    test(axios): test the http-request-api from the library axios

--- frontend/vue2js/src/views/test/index.vue
+++ frontend/vue2js/src/views/test/index.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
-    <p>ssss</p>
-    <pre> you will see info below</pre>
-    <p>{{ info }}</p>
+    <el-button type="primary" @click.native="Call_Info"
+      >Get information from backend</el-button
+    >
+    <el-button type="warning" @click.native="info=null">Reset</el-button>
+    <pre> you will see info below: </pre>
+    <pre>{{ info }}</pre>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'test',
   data() {
     return {
-      info: 'Message'
+      info: 'initial state'
+    }
+  },
+  methods: {
+    Call_Info() {
+      axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then((response) => (this.info = response.data.bpi))
+      console.log('aaa')
     }
   }
 }
