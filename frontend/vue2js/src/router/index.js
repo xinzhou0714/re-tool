64a534ba763a9a116be37d774c0c198f60df5f3b
xinzhou0714 <zhouxin0714@gmail.com>
2022-02-06 10:35:22 +0100

    test(frontend): create page test.vue for testing

--- frontend/vue2js/src/router/index.js
+++ frontend/vue2js/src/router/index.js
@@ -3,7 +3,14 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/test',
+    name: 'test',
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/test/index')
+  }
+]
 
 const router = new VueRouter({
   routes
---
+++ frontend/vue2js/src/views/test/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>ssss</p>
+    <pre> you will see info below</pre>
+    <p>{{ info }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data() {
+    return {
+      info: 'Message'
+    }
+  }
+}
+</script>
+
+<style scoped></style>
